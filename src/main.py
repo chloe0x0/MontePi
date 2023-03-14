d903f6557ea5a747e8d2e364a351ab53a12f7d50
chloe0x0 <ChloeZeiter@gmail.com>
2023-03-14 11:27:12 -0700

    Removed unessecary type check in main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ from montepi import monte_pi
 
 if __name__ == "__main__":
     points = int(input("Enter the number of points to use for the approximation: "))
-    if not isinstance(points, int):
-        raise ValueError("Points must be an integer type")
 
     pi = monte_pi(points)
 
